ec2c5599917790c9c800c30c9c9bb8239c6b297c
ponochovny <ponochovny@gmail.com>
2025-12-02 23:11:06 +0200

    test

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-	// output: 'export',
-	// assetPrefix: 'https://ponochovny.github.io/block-reveal-text/',
-	// images: {
-	// 	unoptimized: true,
-	// },
+	output: 'export',
+	assetPrefix: 'https://ponochovny.github.io/block-reveal-text/',
+	images: {
+		unoptimized: true,
+	},
 }
 
 export default nextConfig
