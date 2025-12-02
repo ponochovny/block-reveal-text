b5507721d6c1e1a3bb1298a52f90e23399ccef36
ponochovny <ponochovny@gmail.com>
2025-12-02 23:19:47 +0200

    revert: deploy setup

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-	output: 'export',
-	assetPrefix: 'https://ponochovny.github.io/block-reveal-text/',
-	images: {
-		unoptimized: true,
-	},
-}
+const nextConfig: NextConfig = {}
 
 export default nextConfig
